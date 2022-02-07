da70009b47be4bfbb9739c5b960c9f95a93e9fff
rrrrodrrrrigo <yectocsalev"gmail.com>
2022-02-07 17:24:24 -0600

    changes

--- sketch-about.js
+++ sketch-about.js
@@ -23,7 +23,7 @@ function preload() {
 
 function setup() {
 
-    createCanvas(windowWidth, windowHeight);
+    createCanvas(windowWidth, windowHeight * 2);
     ton_w = width;
     frameRate(fr);
 
